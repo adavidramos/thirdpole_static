36da182452c0b5024fcf584331a12abea8194cca
David Ramos <dave@imaginaryterrain.com>
2014-08-21 12:58:39 -0400

    filter sidebar

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,21 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(".results-filter li").not(":has(:checked)").hide();
+$(".results-filter .less").hide();
+
+$("#results-filter-topic h4, #results-filter-topic a").click(function() {
+	$("#results-filter-topic li").not(":has(:checked)").toggle(200);
+	$("#results-filter-topic p").toggle(200);
+});
+
+$("#results-filter-organization h4, #results-filter-organization a").click(function() {
+	$("#results-filter-organization li").not(":has(:checked)").toggle(200);
+	$("#results-filter-organization p").toggle(200);
+});
+
+$("#results-filter-coverage h4, #results-filter-coverage a").click(function() {
+	$("#results-filter-coverage li").not(":has(:checked)").toggle(200);
+	$("#results-filter-coverage p").toggle(200);
+});
